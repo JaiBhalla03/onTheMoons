f134994c42bccf5725661747d226430be7cfc581
jai <jai.bhalla2021@vitstudent.ac.in>
2023-07-04 23:40:31 +0530

    Adding the navigation to the navbar

--- components/NavIcon.tsx
+++ components/NavIcon.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import {AiOutlineHome} from "react-icons/ai";
+import {IconType} from "react-icons";
 
-const NavIcon = () => {
+type navIconProps = {
+    heading: string;
+    icon: IconType
+}
+
+
+const NavIcon: React.FC<navIconProps> = ({heading, icon}) => {
+    const iconStyle = {
+        fontSize: '1.5rem'
+    }
     return (
         <div className={'flex flex-col items-center justify-center'}>
-            <AiOutlineHome size={25}/>
-            <div className={'text-lg'}>Home</div>
+            {React.createElement(icon, {style: iconStyle})}
+            <div className={'text-lg'}>{heading}</div>
         </div>
     );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import NavIcon from "@/components/NavIcon";
+import {AiOutlineHome, AiOutlineUser} from "react-icons/ai";
+import {BsCalendarEvent} from "react-icons/bs";
+import Link from "next/link";
 
 const Navbar = () => {
     return (
         <nav className={'fixed bottom-0 w-full px-6 text-gray-500 py-4 bg-white rounded-t-3xl shadow shadow-2xl shadow-black'}>
             <ul className={'flex w-full justify-between'}>
-                <NavIcon/>
-                <NavIcon/>
-                <NavIcon/>
+                <Link href={'/'}>
+                    <NavIcon heading={'Home'} icon={AiOutlineHome}/>
+                </Link>
+                <Link href={'/events'}>
+                    <NavIcon heading={'Events'} icon={BsCalendarEvent}/>
+                </Link>
+                <Link href={'/profile'}>
+                    <NavIcon heading={'Profile'} icon={AiOutlineUser}/>
+                </Link>
             </ul>
         </nav>
     );
