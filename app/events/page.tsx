f886e80564c7d9ead9bef14ed3773a0e3d922c06
jai <jai.bhalla2021@vitstudent.ac.in>
2023-07-05 12:18:00 +0530

    Adding the events page

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import EventNav from "@/components/EventNav";
+import EventCard from "@/components/EventCard";
 
 const Page = () => {
     return (
-        <div>
-            this is the events
-        </div>
+        <main className={''}>
+            <EventNav/>
+            <div className={'mx-2 flex justify-around items-center my-2 border rounded-lg p-1 border-[#6e41e2] bg-gray-200'}>
+                <div className={'bg-[#6e41e2] text-white p-1 rounded-lg flex items-center gap-1'}>
+                    <div>Upcoming Events</div>
+                    <div className={'bg-white flex items-center justify-center rounded-full text-[#6e41e2] w-4 h-4'}>1</div>
+                </div>
+                <div className={'font-bold text-black'}>Joined Events</div>
+            </div>
+            <div className={'px-4 flex flex-col gap-6 pt-4 pb-28'}>
+                <EventCard/>
+                <EventCard/>
+            </div>
+        </main>
     );
 };
 
---
+++ components/EventNav.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {AiFillHeart, AiOutlineSearch} from "react-icons/ai";
+
+const HomeNav = () => {
+    return (
+        <nav className={'bg-[#6e41e2] p-4 flex w-full justify-center text-white'}>
+            <div className={'text-lg flex items-center gap-2'}><AiFillHeart/>ontheMoons</div>
+        </nav>
+    );
+};
+
+export default HomeNav;
